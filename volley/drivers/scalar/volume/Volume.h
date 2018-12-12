ed4b5359314baf60b3419e9e89d4cdae08c795e6
Greg Johnson <greg.p.johnson@intel.com>
2018-12-12 15:03:36 -0600

    work in progress on scalar driver volume implementations.

---
+++ volley/drivers/scalar/volume/Volume.h
@@ -0,0 +1,46 @@
+// ======================================================================== //
+// Copyright 2018 Intel Corporation                                         //
+//                                                                          //
+// Licensed under the Apache License, Version 2.0 (the "License");          //
+// you may not use this file except in compliance with the License.         //
+// You may obtain a copy of the License at                                  //
+//                                                                          //
+//     http://www.apache.org/licenses/LICENSE-2.0                           //
+//                                                                          //
+// Unless required by applicable law or agreed to in writing, software      //
+// distributed under the License is distributed on an "AS IS" BASIS,        //
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. //
+// See the License for the specific language governing permissions and      //
+// limitations under the License.                                           //
+// ======================================================================== //
+
+#pragma once
+
+#include "common/ManagedObject.h"
+#include "common/objectFactory.h"
+
+namespace volley {
+
+  namespace scalar_driver {
+
+    struct Volume : public ManagedObject
+    {
+      Volume()                   = default;
+      virtual ~Volume() override = default;
+
+      static Volume *createInstance(const std::string &type)
+      {
+        return createInstanceHelper<Volume, VLY_VOLUME>(type);
+      }
+
+      virtual void commit() override
+      {
+        ManagedObject::commit();
+      }
+    };
+
+#define VLY_REGISTER_VOLUME(InternalClass, external_name) \
+  VLY_REGISTER_OBJECT(Volume, volume, InternalClass, external_name)
+
+  }  // namespace scalar_driver
+}  // namespace volley

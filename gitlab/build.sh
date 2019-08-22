ee15c4b458458cb25d714de906eab322a54f552c
Jefferson Amstutz <jefferson.d.amstutz@intel.com>
2019-08-21 20:36:38 -0500

    add more test jobs (Ubuntu, Arch, macOS), use the superbuild for everything

--- gitlab/build.sh
+++ gitlab/build.sh
@@ -1,6 +1,30 @@
 #!/bin/bash
+## ======================================================================== ##
+## Copyright 2019 Intel Corporation                                         ##
+##                                                                          ##
+## Licensed under the Apache License, Version 2.0 (the "License");          ##
+## you may not use this file except in compliance with the License.         ##
+## You may obtain a copy of the License at                                  ##
+##                                                                          ##
+##     http://www.apache.org/licenses/LICENSE-2.0                           ##
+##                                                                          ##
+## Unless required by applicable law or agreed to in writing, software      ##
+## distributed under the License is distributed on an "AS IS" BASIS,        ##
+## WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. ##
+## See the License for the specific language governing permissions and      ##
+## limitations under the License.                                           ##
+## ======================================================================== ##
 
 mkdir build
 cd build
-cmake ..
-make -j `nproc`
+
+# NOTE(jda) - Some Linux OSs need to have TBB on LD_LIBRARY_PATH at build time
+export LD_LIBRARY_PATH=`pwd`/install/tbb/lib:${LD_LIBRARY_PATH}
+
+cmake --version
+
+cmake \
+  -DBUILD_JOBS=`nproc` \
+  "$@" ../superbuild
+
+cmake --build .

991dad0cffd4ae3a39242736caa8b2314893304b
Jefferson Amstutz <jefferson.d.amstutz@intel.com>
2019-08-22 22:14:04 -0500

    implement C version of vklTutorial (C++ version is now vklTutorialCpp)

---
+++ apps/tutorials/vklTutorial.c
@@ -0,0 +1,62 @@
+// ======================================================================== //
+// Copyright 2018 Intel Corporation                                         //
+//                                                                          //
+// Licensed under the Apache License, Version 2.0 (the "License");          //
+// you may not use this file except in compliance with the License.         //
+// You may obtain a copy of the License at                                  //
+//                                                                          //
+//     http://www.apache.org/licenses/LICENSE-2.0                           //
+//                                                                          //
+// Unless required by applicable law or agreed to in writing, software      //
+// distributed under the License is distributed on an "AS IS" BASIS,        //
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. //
+// See the License for the specific language governing permissions and      //
+// limitations under the License.                                           //
+// ======================================================================== //
+
+#include <openvkl/openvkl.h>
+#include <stdio.h>
+
+int main()
+{
+  vklLoadModule("ispc_driver");
+
+  VKLDriver driver = vklNewDriver("ispc_driver");
+  vklCommitDriver(driver);
+  vklSetCurrentDriver(driver);
+
+  int dimensions[] = {128, 128, 128};
+
+  const int numVoxels = dimensions[0] * dimensions[1] * dimensions[2];
+
+  VKLVolume volume = vklNewVolume("structured_regular");
+  vklSet3i(volume, "dimensions", dimensions[0], dimensions[1], dimensions[2]);
+  vklSet3f(volume, "gridOrigin", 0, 0, 0);
+  vklSet3f(volume, "gridSpacing", 1, 1, 1);
+
+  float *voxels = malloc(numVoxels * sizeof(float));
+
+  for (int k = 0; k < dimensions[2]; k++)
+    for (int j = 0; j < dimensions[1]; j++)
+      for (int i = 0; i < dimensions[0]; i++)
+        voxels[k * dimensions[0] * dimensions[1] + j * dimensions[2] + i] =
+            (float)i;
+
+  VKLData voxelData = vklNewData(numVoxels, VKL_FLOAT, voxels, 0);
+  vklSetData(volume, "voxelData", voxelData);
+  vklRelease(voxelData);
+
+  vklCommit(volume);
+
+  float objectCoordinates[] = {1.f, 1.f, 1.f};
+
+  float sample = vklComputeSample(volume, (const vkl_vec3f*)objectCoordinates);
+
+  printf("%f\n", sample);
+
+  vklShutdown();
+
+  free(voxels);
+
+  return 0;
+}

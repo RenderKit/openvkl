9c9cb9e982f4b997121eabfe96949acad73e92cb
Tim Rowley <timothy.o.rowley@intel.com>
2019-11-04 16:09:32 -0600

    switch superbuild to ospcommon devel

--- superbuild/dependencies/dep_ospcommon.cmake
+++ superbuild/dependencies/dep_ospcommon.cmake
@@ -1,5 +1,5 @@
 ## ======================================================================== ##
-## Copyright 2019 Intel Corporation                                         ##
+## Copyright 2009-2019 Intel Corporation                                    ##
 ##                                                                          ##
 ## Licensed under the Apache License, Version 2.0 (the "License");          ##
 ## you may not use this file except in compliance with the License.         ##
@@ -21,17 +21,14 @@ if (INSTALL_IN_SEPARATE_DIRECTORIES)
   set(COMPONENT_PATH ${INSTALL_DIR_ABSOLUTE}/${COMPONENT_NAME})
 endif()
 
-set(ENV{OSPCOMMON_TBB_ROOT} ${TBB_PATH})
-
 ExternalProject_Add(${COMPONENT_NAME}
   PREFIX ${COMPONENT_NAME}
   DOWNLOAD_DIR ${COMPONENT_NAME}
   STAMP_DIR ${COMPONENT_NAME}/stamp
   SOURCE_DIR ${COMPONENT_NAME}/src
   BINARY_DIR ${COMPONENT_NAME}/build
-  URL "https://github.com/ospray/ospcommon/archive/v${BUILD_OSPCOMMON_VERSION}.zip"
+  URL "https://github.com/ospray/ospcommon/archive/${BUILD_OSPCOMMON_VERSION}.zip"
   CMAKE_ARGS
-    -DBUILD_TESTING=OFF
     -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
     -DCMAKE_INSTALL_PREFIX:PATH=${COMPONENT_PATH}
     -DCMAKE_INSTALL_INCLUDEDIR=${CMAKE_INSTALL_INCLUDEDIR}
@@ -39,12 +36,14 @@ ExternalProject_Add(${COMPONENT_NAME}
     -DCMAKE_INSTALL_DOCDIR=${CMAKE_INSTALL_DOCDIR}
     -DCMAKE_INSTALL_BINDIR=${CMAKE_INSTALL_BINDIR}
     -DCMAKE_BUILD_TYPE=Release
+    -DINSTALL_DEPS=OFF
+    -DBUILD_TESTING=OFF
     $<$<BOOL:${BUILD_TBB_FROM_SOURCE}>:-DOSPCOMMON_TBB_ROOT=${TBB_PATH}>
   BUILD_COMMAND ${DEFAULT_BUILD_COMMAND}
-  BUILD_ALWAYS OFF
+  BUILD_ALWAYS ${ALWAYS_REBUILD}
 )
 
-set(OSPCOMMON_PATH "${COMPONENT_PATH}/${CMAKE_INSTALL_LIBDIR}/cmake/ospcommon-${BUILD_OSPCOMMON_VERSION}")
+list(APPEND CMAKE_PREFIX_PATH ${COMPONENT_PATH})
 
 if (BUILD_TBB_FROM_SOURCE)
   ExternalProject_Add_StepDependencies(${COMPONENT_NAME} configure tbb)

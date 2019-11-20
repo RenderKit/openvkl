42cba80f46463e6a2bbd9ac5e18c568f54213e2b
Greg Johnson <greg.p.johnson@intel.com>
2019-11-20 10:44:49 -0600

    functional tests: vklIterateHit[4,8,16]() calls should only write hit data for active lanes.

--- testing/apps/tests/vectorized_hit_iterator.cpp
+++ testing/apps/tests/vectorized_hit_iterator.cpp
@@ -246,4 +246,251 @@ TEST_CASE("Vectorized hit iterator", "[hit_iterators]")
       }
     }
   }
+
+  SECTION("only write hits for active lanes")
+  {
+    // will be used to initialize all members of hit struct
+    constexpr float initialHitValue = 999999.f;
+
+    VKLValueSelector valueSelector = vklNewValueSelector(vklVolume);
+
+    std::vector<float> isoValues;
+
+    for (float f = 0.1f; f < 1.f; f += 0.1f) {
+      isoValues.push_back(f);
+    }
+
+    vklValueSelectorSetValues(
+        valueSelector, isoValues.size(), isoValues.data());
+
+    vklCommit(valueSelector);
+
+    vkl_box3f bbox = vklGetBoundingBox(vklVolume);
+
+    std::random_device rd;
+    std::mt19937 eng(rd());
+
+    std::uniform_real_distribution<float> distX(bbox.lower.x, bbox.upper.x);
+    std::uniform_real_distribution<float> distY(bbox.lower.y, bbox.upper.y);
+
+    const int maxWidth = 16;
+
+    std::array<int, 3> nativeWidths{4, 8, 16};
+
+    for (int width = 1; width < maxWidth; width++) {
+      std::vector<vec3f> origins(width);
+      std::vector<vec3f> directions(width);
+      std::vector<vkl_range1f> tRanges(width);
+
+      for (int i = 0; i < width; i++) {
+        origins[i]    = vec3f(distX(eng), distY(eng), -1.f);
+        directions[i] = vec3f(0.f, 0.f, 1.f);
+        tRanges[i]    = vkl_range1f{0.f, inf};
+      }
+
+      for (auto callingWidth : nativeWidths) {
+        if (width > callingWidth || callingWidth != nativeSIMDWidth) {
+          continue;
+        }
+
+        std::vector<int> valid(callingWidth, 0);
+        std::fill(valid.begin(), valid.begin() + width, 1);
+
+        std::vector<float> originsSOA = AOStoSOA_vec3f(origins, callingWidth);
+        std::vector<float> directionsSOA =
+            AOStoSOA_vec3f(directions, callingWidth);
+        std::vector<float> tRangesSOA = AOStoSOA_range1f(tRanges, callingWidth);
+
+        if (callingWidth == 4) {
+          VKLHitIterator4 iterator;
+          vklInitHitIterator4(valid.data(),
+                              &iterator,
+                              vklVolume,
+                              (const vkl_vvec3f4 *)originsSOA.data(),
+                              (const vkl_vvec3f4 *)directionsSOA.data(),
+                              (const vkl_vrange1f4 *)tRangesSOA.data(),
+                              valueSelector);
+
+          VKLHit4 hit;
+
+          // initialize hit values; we'll compare after iteration to ensure
+          // data was not overridden for inactive lanes
+          for (int i = 0; i < 4; i++) {
+            hit.t[i]      = initialHitValue;
+            hit.sample[i] = initialHitValue;
+          }
+
+          int result[4];
+
+          int hitCount = 0;
+
+          while (true) {
+            vklIterateHit4(valid.data(), &iterator, &hit, result);
+
+            int resultSum = 0;
+
+            for (int i = 0; i < width; i++) {
+              resultSum += result[i];
+            }
+
+            // we should have the same result for all active lanes
+            REQUIRE((resultSum == width || resultSum == 0));
+
+            for (int i = 0; i < callingWidth; i++) {
+              INFO("hit iteration "
+                   << hitCount << " lane[" << i << "] valid = " << valid[i]
+                   << ", t = " << hit.t[i] << ", sample =" << hit.sample[i]);
+
+              if (valid[i]) {
+                // should have hit values overridden
+                REQUIRE(hit.t[i] != initialHitValue);
+                REQUIRE(hit.sample[i] != initialHitValue);
+              } else {
+                // should NOT have hit values overridden
+                REQUIRE(hit.t[i] == initialHitValue);
+                REQUIRE(hit.sample[i] == initialHitValue);
+              }
+            }
+
+            if (!result[0]) {
+              break;
+            }
+
+            hitCount++;
+          }
+
+          // ensure at least some hits were returned
+          REQUIRE(hitCount > 0);
+        }
+
+        else if (callingWidth == 8) {
+          VKLHitIterator8 iterator;
+          vklInitHitIterator8(valid.data(),
+                              &iterator,
+                              vklVolume,
+                              (const vkl_vvec3f8 *)originsSOA.data(),
+                              (const vkl_vvec3f8 *)directionsSOA.data(),
+                              (const vkl_vrange1f8 *)tRangesSOA.data(),
+                              valueSelector);
+
+          VKLHit8 hit;
+
+          // initialize hit values; we'll compare after iteration to ensure
+          // data was not overridden for inactive lanes
+          for (int i = 0; i < 8; i++) {
+            hit.t[i]      = initialHitValue;
+            hit.sample[i] = initialHitValue;
+          }
+
+          int result[8];
+
+          int hitCount = 0;
+
+          while (true) {
+            vklIterateHit8(valid.data(), &iterator, &hit, result);
+
+            int resultSum = 0;
+
+            for (int i = 0; i < width; i++) {
+              resultSum += result[i];
+            }
+
+            // we should have the same result for all active lanes
+            REQUIRE((resultSum == width || resultSum == 0));
+
+            for (int i = 0; i < callingWidth; i++) {
+              INFO("hit iteration "
+                   << hitCount << " lane[" << i << "] valid = " << valid[i]
+                   << ", t = " << hit.t[i] << ", sample =" << hit.sample[i]);
+
+              if (valid[i]) {
+                // should have hit values overridden
+                REQUIRE(hit.t[i] != initialHitValue);
+                REQUIRE(hit.sample[i] != initialHitValue);
+              } else {
+                // should NOT have hit values overridden
+                REQUIRE(hit.t[i] == initialHitValue);
+                REQUIRE(hit.sample[i] == initialHitValue);
+              }
+            }
+
+            if (!result[0]) {
+              break;
+            }
+
+            hitCount++;
+          }
+
+          // ensure at least some hits were returned
+          REQUIRE(hitCount > 0);
+        }
+
+        else if (callingWidth == 16) {
+          VKLHitIterator16 iterator;
+          vklInitHitIterator16(valid.data(),
+                               &iterator,
+                               vklVolume,
+                               (const vkl_vvec3f16 *)originsSOA.data(),
+                               (const vkl_vvec3f16 *)directionsSOA.data(),
+                               (const vkl_vrange1f16 *)tRangesSOA.data(),
+                               valueSelector);
+
+          VKLHit16 hit;
+
+          // initialize hit values; we'll compare after iteration to ensure
+          // data was not overridden for inactive lanes
+          for (int i = 0; i < 16; i++) {
+            hit.t[i]      = initialHitValue;
+            hit.sample[i] = initialHitValue;
+          }
+
+          int result[16];
+
+          int hitCount = 0;
+
+          while (true) {
+            vklIterateHit16(valid.data(), &iterator, &hit, result);
+
+            int resultSum = 0;
+
+            for (int i = 0; i < width; i++) {
+              resultSum += result[i];
+            }
+
+            // we should have the same result for all active lanes
+            REQUIRE((resultSum == width || resultSum == 0));
+
+            for (int i = 0; i < callingWidth; i++) {
+              INFO("hit iteration "
+                   << hitCount << " lane[" << i << "] valid = " << valid[i]
+                   << ", t = " << hit.t[i] << ", sample =" << hit.sample[i]);
+
+              if (valid[i]) {
+                // should have hit values overridden
+                REQUIRE(hit.t[i] != initialHitValue);
+                REQUIRE(hit.sample[i] != initialHitValue);
+              } else {
+                // should NOT have hit values overridden
+                REQUIRE(hit.t[i] == initialHitValue);
+                REQUIRE(hit.sample[i] == initialHitValue);
+              }
+            }
+
+            if (!result[0]) {
+              break;
+            }
+
+            hitCount++;
+          }
+
+          // ensure at least some hits were returned
+          REQUIRE(hitCount > 0);
+        }
+
+        else {
+          throw std::runtime_error("unsupported calling width");
+        }
+      }
+    }
+  }
 }

9dd41881ec550aaf49ccfcd62aeb14b47c9c4f85
Krzysztof Raszkowski <krzysztof.raszkowski@intel.com>
2020-01-16 15:36:01 +0000

    ci: add server url to protex script

--- gitlab/source-scan-protex.sh
+++ gitlab/source-scan-protex.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#!/bin/bash
 
 PROTEX_ROOT=/NAS/tools/ip_protex/protex_v7.8
 BDSTOOL=$PROTEX_ROOT/bin/bdstool
+SERVER_URL=https://amrprotex008.devtools.intel.com/
 PROTEX_PROJECT_NAME=c_openvkl_20491
 SRC_PATH=$CI_PROJECT_DIR/
 
@@ -11,12 +11,12 @@ export _JAVA_OPTIONS=-Duser.home=$PROTEX_ROOT/home
 # enter source code directory before scanning
 cd $SRC_PATH
 
-$BDSTOOL new-project $PROTEX_PROJECT_NAME |& tee ip_protex.log
+$BDSTOOL new-project --server $SERVER_URL $PROTEX_PROJECT_NAME |& tee ip_protex.log
 if grep -q "fail\|error\|fatal\|not found" ip_protex.log; then
     exit 1
 fi
 
-$BDSTOOL analyze |& tee -a ip_protex.log
+$BDSTOOL analyze --server $SERVER_URL |& tee -a ip_protex.log
 if grep -q "fail\|error\|fatal\|not found" ip_protex.log; then
     exit 1
 fi

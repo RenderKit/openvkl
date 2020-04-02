ded901a85af0e76a1b787da914093729c87008d1
Greg Johnson <greg.p.johnson@intel.com>
2020-04-02 11:03:48 -0500

    openvklConfig.cmake no longer requiring ospcommon.

--- gitlab/build-from-install.sh
+++ gitlab/build-from-install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ## ======================================================================== ##
-## Copyright 2019 Intel Corporation                                         ##
+## Copyright 2019-2020 Intel Corporation                                    ##
 ##                                                                          ##
 ## Licensed under the Apache License, Version 2.0 (the "License");          ##
 ## you may not use this file except in compliance with the License.         ##
@@ -18,9 +18,6 @@
 export ROOT_DIR=`pwd`
 
 export openvkl_DIR="${ROOT_DIR}/build/install/lib/cmake"
-
-export ospcommon_DIR="${ROOT_DIR}/build/install/lib/cmake"
-export OSPCOMMON_TBB_ROOT="${ROOT_DIR}/build/install"
 
 mkdir build_from_install
 cd build_from_install

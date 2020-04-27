b04146309bff342b3dccd6fb02af7b413722cc98
Krzysztof Raszkowski <krzysztof.raszkowski@intel.com>
2020-04-27 18:35:05 +0000

    ci: change in klocwork scans

--- gitlab/build-kw-scan.sh
+++ gitlab/build-kw-scan.sh
@@ -1,37 +1,5 @@
 #!/bin/bash -x
 
-# function repeat the command $1 times, when the last line output is equal ERROR_MSG
-function retry_cmd()
-{
-    set +e
-    MAX_RETRY=$1
-    CMD="${@:2}"
-    ERROR_MSG="License check failed ... Exiting"
-
-    RETRY_COUNTER="0"
-    while [ $RETRY_COUNTER -lt $MAX_RETRY ]; do
-        CMD_OUTPUT=$($CMD)
-        CMD_RETURN_CODE=$?
-        echo "$CMD_OUTPUT"
-        CMD_OUTPUT=$(echo $CMD_OUTPUT | tail -1)
-        if [ $CMD_RETURN_CODE == 0 ] && [[ $CMD_OUTPUT != $ERROR_MSG ]]; then
-            break
-        elif [ $CMD_RETURN_CODE != 1 ]; then
-            set -e
-            echo "Unknown script error code = [$CMD_RETURN_CODE]"
-            return $CMD_RETURN_CODE
-        fi
-        RETRY_COUNTER=$[$RETRY_COUNTER+1]
-        echo "Found license check failed, [$RETRY_COUNTER/$MAX_RETRY] - retrying ... "
-        sleep 10
-    done
-
-    set -e
-    if [ $RETRY_COUNTER -ge $MAX_RETRY ]; then
-        return 62
-    fi
-    return 0
-}
 set -e
 KW_PATH=/NAS/tools/kw
 KW_SERVER_PATH=$KW_PATH/server
@@ -69,8 +37,7 @@ cmake \
 
 # build
 $KW_CLIENT_PATH/bin/kwinject make -j `nproc`
-
-retry_cmd 60 $KW_SERVER_PATH/bin/kwbuildproject --url http://$KW_SERVER_IP:$KW_SERVER_PORT/$KW_PROJECT_NAME --tables-directory $CI_PROJECT_DIR/kw_tables kwinject.out
-retry_cmd 60 $KW_SERVER_PATH/bin/kwadmin --url http://$KW_SERVER_IP:$KW_SERVER_PORT/ load $KW_PROJECT_NAME $CI_PROJECT_DIR/kw_tables | tee project_load_log
-cat project_load_log | grep "Starting build" | cut -d":" -f2 > $CI_PROJECT_DIR/kw_build_number
+$KW_SERVER_PATH/bin/kwbuildproject --url http://$KW_SERVER_IP:$KW_SERVER_PORT/$KW_PROJECT_NAME --tables-directory $CI_PROJECT_DIR/kw_tables kwinject.out
+$KW_SERVER_PATH/bin/kwadmin --url http://$KW_SERVER_IP:$KW_SERVER_PORT/ load --force --name build-$CI_JOB_ID $KW_PROJECT_NAME $CI_PROJECT_DIR/kw_tables
+echo "build-$CI_JOB_ID" > $CI_PROJECT_DIR/kw_build_number
 

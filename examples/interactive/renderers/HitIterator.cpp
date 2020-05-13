f499f95aa15dfc95f70e8593340c21a93bb4a334
Johannes Meng <johannes.meng@intel.com>
2020-05-13 18:27:48 +0200

    Use gradients for surface shading in the HitIterator example renderer

--- examples/interactive/renderers/HitIterator.cpp
+++ examples/interactive/renderers/HitIterator.cpp
@@ -13,7 +13,7 @@ namespace openvkl {
       ispcEquivalent = ispc::HitIterator_create();
     }
 
-    vec3f HitIterator::renderPixel(const Scene& scene, Ray &ray, const vec4i &)
+    vec3f HitIterator::renderPixel(const Scene &scene, Ray &ray, const vec4i &)
     {
       vec3f color(0.f);
       float alpha = 0.f;
@@ -36,12 +36,59 @@ namespace openvkl {
 
       // the current surface hit
       VKLHit hit;
+      const float surfaceAlpha = 0.6f;
+      const float emission[]   = {1.f, 0.8f};
+      const vec3f lightDir[]   = {normalize(vec3f(1.f, 1.f, 1.f)),
+                                normalize(vec3f(1.f, 1.f, -1.f))};
 
       while (vklIterateHit(&iterator, &hit) && alpha < 0.99f) {
-        vec4f surfaceColorAndOpacity = sampleTransferFunction(scene, hit.sample);
+        const vec3f c = ray.org + hit.t * ray.dir;
+        const vkl_vec3f grad =
+            vklComputeGradient(scene.sampler, (vkl_vec3f *)&c);
+        vec3f N = normalize(vec3f(grad.x, grad.y, grad.z));
+        if (std::isnan(N.x) || std::isnan(N.y) || std::isnan(N.z))
+          N = vec3f(0.f);
+        // wi and wo both point away from the event.
+        const vec3f wi = normalize(-1.f * ray.dir);
+        const float ci = dot(N, wi);
 
-        color = color + (1.f - alpha) * vec3f(surfaceColorAndOpacity);
-        alpha = alpha + (1.f - alpha) * 0.25f;
+        float illum = 0.f;
+        if (length(N) > 0) {
+          illum = 0.1f;  // Ambient term.
+          // Passing through the surface? Include the surface we started on in
+          // alpha.
+          for (int i = 0; i < 2; ++i) {
+            const vec3f wo = lightDir[i];
+            const float co = dot(N, wo);
+
+            // Only test for shadow if we don't have to go through this surface.
+            if ((co > 0) == (ci > 0)) {
+              VKLHit shadowHit;
+              VKLHitIterator shadowIterator;
+              vkl_range1f tShadowRange;
+              tShadowRange.lower = 0.001f;
+              tShadowRange.upper = inf;
+              vklInitHitIterator(&shadowIterator,
+                                 scene.volume,
+                                 (vkl_vec3f *)&c,
+                                 (vkl_vec3f *)&wo,
+                                 &tShadowRange,
+                                 scene.valueSelector);
+              if (!vklIterateHit(&shadowIterator, &shadowHit)) {
+                illum += abs(co) * emission[i];  // Lambertian surface shading.
+              }
+            }
+          }
+        } else {
+          // We have no normal, so do not attempt to shade.
+          illum = 1.f;
+        }
+        // Lambertian surface shading.
+        vec4f surfaceColorAndOpacity =
+            sampleTransferFunction(scene, hit.sample);
+        const vec3f albedo = vec3f(surfaceColorAndOpacity);
+        color              = color + (1 - alpha) * illum * albedo;
+        alpha              = alpha + (1.f - alpha) * surfaceAlpha;
       }
 
       return color;

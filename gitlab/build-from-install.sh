320b86acf7dd20fad102ce72783fbeaa0c63db2b
Greg Johnson <greg.p.johnson@intel.com>
2020-06-22 13:40:32 -0500

    update linkage to rkcommon.

--- gitlab/build-from-install.sh
+++ gitlab/build-from-install.sh
@@ -3,8 +3,10 @@
 ## SPDX-License-Identifier: Apache-2.0
 
 export ROOT_DIR=`pwd`
+export DEP_INSTALL_DIR="${ROOT_DIR}/build/install"
 
-export openvkl_DIR="${ROOT_DIR}/build/install/lib/cmake"
+export openvkl_DIR=${DEP_INSTALL_DIR}
+export rkcommon_DIR=${DEP_INSTALL_DIR}
 
 mkdir build_from_install
 cd build_from_install
@@ -12,8 +14,10 @@ cd build_from_install
 cmake --version
 
 cmake \
+  -D RKCOMMON_TBB_ROOT=$DEP_INSTALL_DIR \
   "$@" ../examples/from_openvkl_install
 
 cmake --build .
 
 ./vklTutorial
+./vklTutorialLinkISPCDriver

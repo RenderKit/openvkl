2f3977cbdb8b5275d8125dbfa8afac3688e2ce92
Greg Johnson <greg.p.johnson@intel.com>
2020-07-01 12:11:31 -0500

    update linkage to rkcommon.

--- gitlab/build-from-install.sh
+++ gitlab/build-from-install.sh
@@ -6,7 +6,6 @@ export ROOT_DIR=`pwd`
 export DEP_INSTALL_DIR="${ROOT_DIR}/build/install"
 
 export openvkl_DIR=${DEP_INSTALL_DIR}
-export rkcommon_DIR=${DEP_INSTALL_DIR}
 
 mkdir build_from_install
 cd build_from_install
@@ -14,7 +13,6 @@ cd build_from_install
 cmake --version
 
 cmake \
-  -D RKCOMMON_TBB_ROOT=$DEP_INSTALL_DIR \
   "$@" ../examples/from_openvkl_install
 
 cmake --build .

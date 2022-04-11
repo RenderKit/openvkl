d1ea9c9f194b3166ca233c675bd10b90e8fe605f
Greg Johnson <greg.p.johnson@intel.com>
2022-04-11 15:24:55 -0600

    disable fast math when using ICX.

--- cmake/openvkl_macros.cmake
+++ cmake/openvkl_macros.cmake
@@ -56,6 +56,10 @@ macro(openvkl_configure_global_build_flags)
 
      set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fno-strict-aliasing")
 
+     if(CMAKE_CXX_COMPILER_ID STREQUAL "IntelLLVM")
+       set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fp-model=precise")
+     endif()
+
   elseif(CMAKE_CXX_COMPILER_ID STREQUAL "Intel")
     if(NOT WIN32)
       set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -no-ansi-alias")

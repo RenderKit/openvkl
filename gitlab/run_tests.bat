827901a06caf43321c0f518efc22d37c9ed8518d
Greg Johnson <greg.p.johnson@intel.com>
2023-01-18 11:45:00 -0700

    CI: add vklExamplesCPU runs for Linux, Mac, Windows workflows.

--- gitlab/run_tests.bat
+++ gitlab/run_tests.bat
@@ -10,6 +10,7 @@ set PATH=%PATH%;.\build\install\bin
 
 call .\build\install\bin\vklTutorialCPU.exe
 call .\build\install\bin\vklTutorialISPC.exe
+call .\build\install\bin\vklExamplesCPU -batch -printStats -spp 50 -framebufferSize 1024 1024
 call .\build\install\bin\vklTestsCPU.exe
 
 :abort

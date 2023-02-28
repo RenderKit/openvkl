38dbecfbd3c41efeed6b73d1b24c0f01736978d6
Greg Johnson <greg.p.johnson@intel.com>
2023-02-28 06:46:40 -0700

    superbuild: update to ISPC v1.19.0.

--- cmake/openvkl_ispc.cmake
+++ cmake/openvkl_ispc.cmake
@@ -4,7 +4,7 @@
 option(OPENVKL_ISPC_FAST_MATH "enable ISPC fast-math optimizations" OFF)
 
 # ISPC versions to look for, in decending order (newest first)
-set(ISPC_VERSION_WORKING "1.18.0")
+set(ISPC_VERSION_WORKING "1.19.0" "1.18.0")
 list(GET ISPC_VERSION_WORKING -1 ISPC_VERSION_REQUIRED)
 
 if (NOT ISPC_EXECUTABLE)

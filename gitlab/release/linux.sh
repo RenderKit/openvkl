a4c9497398d2eb90a76c7b4cf101e0eb2e32c445
Greg Johnson <greg.p.johnson@intel.com>
2023-09-29 15:00:56 -0600

    CI: move all centos7 builds to rocky87.
    no longer perform explicit symbol checks, since we validate on minimum platforms.

--- gitlab/release/linux.sh
+++ gitlab/release/linux.sh
@@ -2,47 +2,6 @@
 ## Copyright 2020 Intel Corporation
 ## SPDX-License-Identifier: Apache-2.0
 
-#### Helper functions ####
-
-function check_symbols
-{
-  for sym in `nm $1 | grep $2_`
-  do
-    version=(`echo $sym | sed 's/.*@@\(.*\)$/\1/g' | grep -E -o "[0-9]+"`)
-    if [ ${#version[@]} -ne 0 ]; then
-      if [ ${#version[@]} -eq 1 ]; then version[1]=0; fi
-      if [ ${#version[@]} -eq 2 ]; then version[2]=0; fi
-      if [ ${version[0]} -gt $3 ]; then
-        echo "Error: problematic $2 symbol " $sym
-        exit 1
-      fi
-      if [ ${version[0]} -lt $3 ]; then continue; fi
-
-      if [ ${version[1]} -gt $4 ]; then
-        echo "Error: problematic $2 symbol " $sym
-        exit 1
-      fi
-      if [ ${version[1]} -lt $4 ]; then continue; fi
-
-      if [ ${version[2]} -gt $5 ]; then
-        echo "Error: problematic $2 symbol " $sym
-        exit 1
-      fi
-    fi
-  done
-}
-
-function check_imf
-{
-for lib in "$@"
-do
-  if [ -n "`ldd $lib | fgrep libimf.so`" ]; then
-    echo "Error: dependency to 'libimf.so' found"
-    exit 3
-  fi
-done
-}
-
 #### Set variables for script ####
 
 ROOT_DIR=$PWD
@@ -112,23 +71,6 @@ cmake -L \
 
 # build
 make -j $THREADS install
-
-# verify libs
-check_symbols $OPENVKL_INSTALL_DIR/lib/libopenvkl.so GLIBC   2 17 0
-check_symbols $OPENVKL_INSTALL_DIR/lib/libopenvkl.so GLIBCXX 3 4 19
-check_symbols $OPENVKL_INSTALL_DIR/lib/libopenvkl.so CXXABI  1 3 7
-
-check_symbols $OPENVKL_INSTALL_DIR/lib/libopenvkl_module_cpu_device.so GLIBC   2 17 0
-check_symbols $OPENVKL_INSTALL_DIR/lib/libopenvkl_module_cpu_device.so GLIBCXX 3 4 19
-check_symbols $OPENVKL_INSTALL_DIR/lib/libopenvkl_module_cpu_device.so CXXABI  1 3 7
-
-check_symbols $OPENVKL_INSTALL_DIR/lib/libopenvkl_module_cpu_device_4.so GLIBC   2 17 0
-check_symbols $OPENVKL_INSTALL_DIR/lib/libopenvkl_module_cpu_device_4.so GLIBCXX 3 4 19
-check_symbols $OPENVKL_INSTALL_DIR/lib/libopenvkl_module_cpu_device_4.so CXXABI  1 3 7
-
-check_imf $OPENVKL_INSTALL_DIR/lib/libopenvkl.so
-check_imf $OPENVKL_INSTALL_DIR/lib/libopenvkl_module_cpu_device.so
-check_imf $OPENVKL_INSTALL_DIR/lib/libopenvkl_module_cpu_device_4.so
 
 # copy dependent libs into the install
 INSTALL_LIB_DIR=$OPENVKL_INSTALL_DIR/lib
